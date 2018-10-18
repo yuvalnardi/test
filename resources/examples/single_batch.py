813592b1a4486b486095ba17a812509631fd3147
Yuval Nardi <yuval.nardi@gmail.com>
2018-10-19 00:36:25 +0300

    add date_parser to make read_csv faster

--- resources/examples/single_batch.py
+++ resources/examples/single_batch.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import csv
 
@@ -7,33 +6,37 @@ from src.utils.logger import log
 pd.set_option('display.expand_frame_repr', False)
 # pd.set_option('display.max_rows', None, 'display.max_columns', None)
 
-log.debug('Loading single_batch data ..')
-#data = pd.read_csv('/Users/yuval/Downloads/Sensor_readings.csv')
-data = pd.read_csv('/Users/yuval/Downloads/Sensor_readings.csv',
-                   infer_datetime_format=True,
-                   parse_dates=['end_time_stamp'])
-log.debug('Done loading single_batch data.')
+change_timestamp_format = False
 
-print(data.shape)
-print(data.head())
-print(data.dtypes)
+if change_timestamp_format:
+    log.debug('Loading single_batch data ..')
+    date_parser = lambda x: pd.datetime.strptime(x, '%d/%m/%y %H:%M')
+    data = pd.read_csv('/Users/yuval/Downloads/Sensor_readings.csv',
+                       parse_dates=['end_time_stamp'],
+                       infer_datetime_format=True,
+                       date_parser=date_parser)
+    log.debug('Done loading single_batch data.')
 
-# DATE_FORMAT = u'%Y-%m-%d'
-# TIMESTAMP_FORMAT = u'%Y-%m-%d %H:%M:%S'
-#
-# data.to_csv('/Users/yuval/Downloads/Sensor_readings_YUVAL.csv',
-#            index=False,
-#             quoting=csv.QUOTE_ALL,
-#             doublequote=True,
-#             date_format=TIMESTAMP_FORMAT)
-#
-# data = pd.read_csv('/Users/yuval/Downloads/Sensor_readings_YUVAL.csv',
-#                    parse_dates=['end_time_stamp'],
-#                    infer_datetime_format=True)
+    date_format = u'%Y-%m-%d'
+    timestamp_format = u'%Y-%m-%d %H:%M:%S'
+
+    log.debug('Persisting to csv new timestamp format ..')
+    data.to_csv('/Users/yuval/Downloads/Sensor_readings_YUVAL.csv',
+                index=False,
+                quoting=csv.QUOTE_ALL,
+                doublequote=True,
+                date_format=timestamp_format)
+    log.debug('Done persisting to csv new timestamp format.')
+else:
+    log.debug('Loading single_batch data ..')
+    data = pd.read_csv('/Users/yuval/Downloads/Sensor_readings_YUVAL.csv',
+                       parse_dates=['end_time_stamp'],
+                       infer_datetime_format=True)
+    log.debug('Done loading single_batch data.')
 
 min_timestamp_per_sensor = data.groupby('metric_id')['end_time_stamp'].aggregate(lambda ts: ts.min())
 
-min_timestamp_distribution =  min_timestamp_per_sensor.value_counts()
+min_timestamp_distribution = min_timestamp_per_sensor.value_counts()
 min_timestamp_distribution = min_timestamp_distribution.sort_index()
 
 print(min_timestamp_distribution)
