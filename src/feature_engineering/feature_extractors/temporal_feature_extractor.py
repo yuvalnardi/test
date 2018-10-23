89c4bf36b3c1d83f9afa17f7fb689435b6b9cb93
Yuval Nardi <yuval.nardi@gmail.com>
2018-10-24 01:03:59 +0300

    add temporal feature

--- src/feature_engineering/feature_extractors/temporal_feature_extractor.py
+++ src/feature_engineering/feature_extractors/temporal_feature_extractor.py
@@ -10,6 +10,6 @@ class TemporalFeatureExtractor(FeatureExtractorBase):
         super().__init__()
 
     def extract(self, data):
-        design_matrix = pd.DataFrame(np.random.normal(10, 1, [5, 2]), columns=['Z1', 'Z2'])
+        design_matrix = pd.DataFrame(np.random.normal(10, 1, [5, 3]), columns=['Z1', 'Z2', 'Z3'])
 
         return design_matrix
