7d102eaa158457cadff2f33c9169a98321a998c3
Yuval Nardi <yuval.nardi@gmail.com>
2018-10-26 01:28:04 +0300

    pro|retro-spect charts

---
+++ resources/examples/create_charts.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import random
+import matplotlib.pyplot as plt
+
+from src.utils.logger import log
+
+# pd.set_option('display.max_rows', None, 'display.max_columns', None)
+pd.set_option('display.expand_frame_repr', False)
+
+
+def create_prospect_chart(data, batch_id, sensor_id):
+    log.debug('Creating prospect charts (Forward View) for batch {} and sensor {}.'.format(
+        batch_id, sensor_id))
+
+    assert isinstance(data, pd.DataFrame)
+    expected_columns = ['batch_id', 'sensor_id', 'timestamp', 'value', 'batch_label']
+    assert set(data.columns) == set(expected_columns)
+    s = 'batch {} has no records for sensor {}'.format(batch_id, sensor_id)
+    assert sensor_id in data.loc[data['batch_id'] == batch_id, 'sensor_id'].unique(), s
+
+    # TODO:
+    x = data.loc[(data['batch_id'] == batch_id) & (data['sensor_id'] == sensor_id), 'timestamp']
+    y = data.loc[(data['batch_id'] == batch_id) & (data['sensor_id'] == sensor_id), 'value']
+
+    y_average_normal = y + 2
+
+    lines = plt.plot(x, y, 'k-', x, y_average_normal, 'b--')
+    plt.show()
+
+    log.debug('Done creating prospect charts (Forward View) for batch {} and sensor {}.'.format(
+        batch_id, sensor_id))
+
+
+def create_retrospect_chart(data, batch_id, sensor_id):
+    log.debug('Creating retrospect charts (Backward View) for batch {} and sensor {}.'.format(
+        batch_id, sensor_id))
+
+    assert isinstance(data, pd.DataFrame)
+    expected_columns = ['batch_id', 'sensor_id', 'timestamp', 'value', 'batch_label']
+    assert set(data.columns) == set(expected_columns)
+    s = 'batch {} has no records for sensor {}'.format(batch_id, sensor_id)
+    assert sensor_id in data.loc[data['batch_id'] == batch_id, 'sensor_id'].unique(), s
+
+    # TODO:
+
+    log.debug('Done creating retrospect charts (Forward View) for batch {} and sensor {}.'.format(
+        batch_id, sensor_id))
+
+
+if __name__ == '__main__':
+    fullpath = '/Users/yuval/Desktop/mock_data.csv'
+    data = pd.read_csv(fullpath, parse_dates=['timestamp'], infer_datetime_format=True)
+
+    batch_id = random.choice(data['batch_id'].unique())
+    sensor_id = random.choice(data['sensor_id'].unique())
+
+    create_prospect_chart(data, batch_id, sensor_id)
+    #create_retrospect_chart(data, batch_id, sensor_id)
