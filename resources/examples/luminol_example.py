3914bf8321b5f2e0bd3034fa7b9c1fb7a7e63268
Yuval Nardi <yuval.nardi@gmail.com>
2018-10-31 23:10:28 +0200

    playing with luminol

--- resources/examples/luminol_example.py
+++ resources/examples/luminol_example.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import random
 import time
+from luminol.modules.time_series import TimeSeries
+import matplotlib.pyplot as plt
 
 from src.utils.logger import log
 
@@ -10,18 +12,47 @@ pd.set_option('display.expand_frame_repr', False)
 
 from luminol.anomaly_detector import AnomalyDetector
 
-# ts = {0: 0, 1: 0.5, 2: 1, 3: 1, 4: 1, 5: 0, 6: 0, 7: 0, 8: 0}
-ts = {
-    '1490323038': 3,
-    '1490323048': 4,
-    '1490323058': 6,
-    '1490323068': 78,
-    '1490323078': 67,
-    '1490323088': 5,
-}
+# ts = {
+#     '1490323038': 3,
+#     '1490323048': 4,
+#     '1490323058': 6,
+#     '1490323068': 1078,
+#     '1490323078': 1607,
+#     '1490323088': 5,
+# }
+#
+# my_detector = AnomalyDetector(ts)
+# score = my_detector.get_all_scores()
+# anomalies = my_detector.get_anomalies()
+# anom_score = []
+#
+# for (timestamp, value) in score.iteritems():
+#     t_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp))
+#     anom_score.append([t_str, value])
+#
+# for score in anom_score:
+#     print(score)
 
-my_detector = AnomalyDetector(ts)
+
+from luminol.utils import to_epoch
+
+path = '/Users/yuval/Dropbox/MyData/Misc/Seebo/data/g.csv'
+ts = pd.read_csv(path) # , parse_dates=['timestamp'], infer_datetime_format=True)
+ts = ts.iloc[0:10000]
+# convert to epoch (unix time)
+#ts['epoch'] = ts['timestamp'].transform(lambda timestamp: to_epoch(timestamp))
+
+keys = ts['timestamp']
+values = ts['value']
+ts_dict = dict(zip(keys, values))
+
+plt.plot(ts['value'])
+plt.show()
+
+
+my_detector = AnomalyDetector(ts_dict)
 score = my_detector.get_all_scores()
+anomalies = my_detector.get_anomalies()
 anom_score = []
 
 for (timestamp, value) in score.iteritems():
